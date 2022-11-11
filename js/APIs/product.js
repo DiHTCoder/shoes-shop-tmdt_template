00ceb0eb8396c1bae6df67a9c0a5d018b6594ef1
Huynh Tien Di <20110246@student.hcmute.com.vn>
2022-11-11 20:30:24 +0700

    cart

--- js/APIs/product.js
+++ js/APIs/product.js
@@ -7,7 +7,6 @@
         getJsonTop4Product(getTop4Product);
         getProductsFromJson(getAllProduct);
         showCart(cart);
-        getAllTotalItem();
     }
 
     // get data from JSON
@@ -102,23 +101,24 @@
     // addItemToCart(3)
 
     function removeItemFromCart(productId){
-        let temp = cart.filter(element=> element.product.id != productId);
-        localStorage.setItem("cart",JSON.stringify(temp));
+        cart = cart.filter(element=> element.product.id != productId);
+        localStorage.setItem("cart",JSON.stringify(cart));
         showCart(cart);
+        getAllTotalItem(cart);
     }
     // removeItemFromCart(4)
 
     function updateItemCart(productId,quantity){
         for(let product of cart){
-            if(product.id == productId){
+            if(product.product.id == productId){
                 product.quantity = quantity;
             }
         }
         localStorage.setItem("cart",JSON.stringify(cart));
     }
-    // updateItemCart(1,1)
+    // updateItemCart(1,3)
 
-    function getAllTotalItem(){
+    function getAllTotalItem(cart){
         let total = document.getElementById("cart__total");
         let temp = cart.map(function(item){
             return parseFloat(item.product.price*item.quantity);
@@ -130,19 +130,42 @@
     }
     // getAllTotalItem();
 
-
     function getTotalOneItem(price, quantity){
-
-            return parseFloat(price*quantity);
-      
+        return parseFloat(price*quantity);
     }
     // getTotalItem()
-    function refresh() {    
-        location.reload()
+
+    // Tăng số lượng
+    function increaseQuantity(id){
+        let seletedId = id;
+        let search = cart.find((x)=> x.product.id === seletedId);
+        search.quantity+=1;
+        localStorage.setItem("cart",JSON.stringify(cart));
+        showCart(cart);
+        // updateItemCart(seletedId, search);
     }
+
+    // Giảm số lượng
+    function decreaseQuantity(id){
+        let seletedId = id;
+        let search = cart.find((x)=> x.product.id === seletedId);
+        if(search.quantity < 2)
+        {
+            search.quantity =1;
+        }
+        else{
+            search.quantity -=1;
+        }
+
+        localStorage.setItem("cart",JSON.stringify(cart));
+        showCart(cart);
+        // updateItemCart(seletedId, search);
+    }
+    
     function showCart(shoppingCart){
         var cartBody = document.querySelector('.shopping-cart-item');
         let html = shoppingCart.map((cart) => {
+            var totalItem =getTotalOneItem(cart.product.price,cart.quantity);
             return `
                 <tr>
                     <td class="product__cart__item">
@@ -157,17 +180,20 @@
                     <td class="quantity__item">
                         <div class="quantity">
                             <div class="pro-qty-2">
-                                <span class="fa fa-angle-left dec qtybtn"></span>
+                                <span class="fa fa-angle-left dec qtybtn" onclick="decreaseQuantity(${cart.product.id})"></span>
                                 <input type="text" value="${cart.quantity}">
-                                <span class="fa fa-angle-right inc qtybtn"></span>
+                                <span class="fa fa-angle-right inc qtybtn" onclick="increaseQuantity(${cart.product.id})"></span>
                             </div>
                         </div>
                     </td>
-                    <td class="cart__price" value="getTotalOneItem(${cart.product.price},${cart.quantity})"></td>
-                    <td class="cart__close"><button onclick="removeItemFromCart(${cart.product.id})"><i class="fa fa-close"></i></button></td>
+                    <td class="cart__price" >
+                    <p>${totalItem}</p>
+                    </td>
+                    <td class="cart__close"><button onclick="removeItemFromCart(${cart.product.id})" style="border-radius: 25%;"><i class="fa fa-close"></i></button></td>
             `;
         }).join("");
         cartBody.innerHTML = html;
+        getAllTotalItem(cart);
     };
 
     // Get data from JSON top productsTop4
